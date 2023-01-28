e6e6a1b0c3403cc9ebf77de2251dbb5913842efc
funmbia <92063639+funmbia@users.noreply.github.com>
2023-01-28 17:33:36 -0500

    Update spareFood.jsx

--- src/pages/spareFood.jsx
+++ src/pages/spareFood.jsx
@@ -1,8 +1,19 @@
+import './spareFood.css'
 const DUMMY_FOOD=[]
 const SpareFood=()=> {
    if(DUMMY_FOOD.length==0){
     return (
         <h1>There is no spare food, please wait....</h1>
+       <div className="restaurant-container">
+        <div id='title'> 
+            <h1>Spare Meals </h1> 
+            <h5> Combat Food Waste</h5>
+            <p id="slogan"> One Bite at a Time</p>
+        </div>
+ 
+        <div id='content'>
+            <p> Looks like there's currently no one offering SpareMeals :(</p>
+        </div>  
     )
    }
 }
