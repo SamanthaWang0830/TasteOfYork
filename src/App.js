0365e2bda33e732effa1ad136f616f92b82fb4d1
Araf Alam <araf821@Arafs-MacBook-Pro.local>
2023-01-28 15:20:02 -0500

    modified app.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Header/>}>
-        <Route index element={<Login/>}/>
+        <Route index element={<LoginPage/>}/>
         <Route path='findRestaurants' element={<FindRestaurants/>} />
         <Route path="studentsPicks"  element={<StudentsPicks/>} />
         <Route path="spareFood"  element={<SpareFood/>} />
