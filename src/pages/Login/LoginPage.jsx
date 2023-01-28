a4b503eb7c1eda8633d3dc6381bc198965403dc0
Araf Alam <araf821@Arafs-MacBook-Pro.local>
2023-01-28 16:40:01 -0500

    modified login page

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import "./login.css";
 import { TextField, Button } from "@mui/material";
+import { grey } from "@mui/material/colors";
 
 export default function Login() {
+  const white = grey[50];
 
   return (
     <div className="body">
       <div class="login-box">
         <h2>Login / Sign Up</h2>
         <TextField
+          className="textField"
           fullWidth
           required
           id="email"
@@ -17,6 +20,7 @@ export default function Login() {
           sx={{ mb: 2 }}
         />
         <TextField
+          className="textField"
           fullWidth
           required
           id="password"
@@ -25,14 +29,26 @@ export default function Login() {
           variant="outlined"
           sx={{ mb: 2 }}
         />
-        <Button size="large" type="submit" fullWidth variant="contained">
+        <Button
+          className="btn"
+          size="large"
+          type="submit"
+          fullWidth
+          variant="contained"
+        >
           Login
         </Button>
 
-        <Button size="large" type="submit" fullWidth variant="contained" sx={{ mt: 1 }}>
+        <Button
+          className="btn"
+          size="large"
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 1 }}
+        >
           Create Account
         </Button>
-        
       </div>
     </div>
   );
