00df2ed5e850633db9efc48745bfb20c51b400b5
samantha <samantha0830@163.com>
2023-02-14 21:37:19 -0500

    try

---
+++ backend/models/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary= require('cloudinary')
+
+cloudinary.config({
+    cloud_name: "dafxtxhdp",
+    api_key: "179167257993584",
+    api_secret: "HCk7bPhd0KKyzBOxfQnHXnX3ZGI"
+});
+
+module.exports=cloudinary
